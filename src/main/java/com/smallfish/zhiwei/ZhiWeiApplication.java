561a37f5c6e31ad7187435a7fb17ae842843fc66
XiaoYu <2489927141@qq.com>
2026-01-07 14:45:51 +0800

    init: 项目初始化

---
+++ src/main/java/com/smallfish/zhiwei/ZhiWeiApplication.java
@@ -0,0 +1,14 @@
+package com.smallfish.zhiwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+*  项目启动类
+* */
+@SpringBootApplication
+public class ZhiWeiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiWeiApplication.class, args);
+    }
+}
